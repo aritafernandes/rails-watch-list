f18ad593cb8c179518eec2afbe26cdcbc5e726fb
rita fernandes <ritaestevesfernandes@gmail.com>
2023-05-25 22:38:23 +0100

    backend done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+Movie.destroy_all
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+user_serialized = URI.open(url).read
+user = JSON.parse(user_serialized)
+user['results'].each do |movie|
+  Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie['poster_path'], rating: movie['vote_average'])
+end
